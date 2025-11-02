3b0e71180596245be394aa6393d25b3d9c97e1af
sanath <sanath321999@gmail.com>
2025-11-02 21:33:27 +0530

    vpc-setup

---
+++ terraform-project2/modules/vpc/main.tf
@@ -0,0 +1,103 @@
+resource "aws_vpc" "test1" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.test1.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+  tags = {
+    Name = var.subnet_name
+    map_public_ip_on_launch = "true"
+  }
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.test1.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = var.availability_zone
+    tags = {
+        Name = var.subnet_name
+        map_public_ip_on_launch = "true"
+    }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.test1.id
+  tags = {
+    Name = var.igw_name
+  }
+}
+resource "aws_eip" "nat_eip" {
+    domain="vpc"
+}
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_subnet_1.id
+    tags = {
+        Name = var.nat_gw_name
+    }
+    depends_on=[aws_internet_gateway.igw]
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.test1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+    tags = {
+    Name = var.route_table_name
+  }
+}
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.test1.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gw.id
+    }
+    tags = {
+        Name = var.private_route_table_name
+    }
+}
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_route_table_association" "private_rt_assoc_1" {
+    subnet_id = aws_subnet.private_subnet_1.id
+    route_table_id = aws_route_table.private_rt.id
+}
+resource "aws_route_table_association" "private_rt_assoc_2" {
+    subnet_id = aws_subnet.private_subnet_2.id
+    route_table_id = aws_route_table.private_rt.id
+}
+resource "aws_security_group" "vpc_sg" {
+  name        = var.security_group_name
+  description = "Security group for VPC"
+  vpc_id      = aws_vpc.test1.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
